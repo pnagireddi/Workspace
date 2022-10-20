71ca8e7372e53151f136dca914a3b9f3ab947aa6
NagiReddy <pnagireddy444@gmail.com>
2022-10-20 10:36:29 +0530

    Launch 1

---
+++ PatternPrograming/src/Launch1.java
@@ -0,0 +1,20 @@
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+             
+		// to print box of stars.
+		
+		int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
