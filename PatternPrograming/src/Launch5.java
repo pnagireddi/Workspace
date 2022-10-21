21bc1d9c06d586b3709bbd279e2639bc1e326d1a
NagiReddy <pnagireddy444@gmail.com>
2022-10-21 19:26:33 +0530

    launchs second commit

---
+++ PatternPrograming/src/Launch5.java
@@ -0,0 +1,22 @@
+
+public class Launch5 {
+	public static void main(String[] args) {
+
+		// to print E
+
+		int n = 11;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j == 0 || (i == 0 && j <= (3 * n) / 4) || (i == (n - 1) / 2 && j <= (3 * n) / 4)
+						|| (i == (n - 1) && j <= (3 * n) / 4)) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ PatternPrograming/src/Launch7.java
@@ -0,0 +1,23 @@
+
+public class Launch7 {
+
+	public static void main(String[] args) {
+     // to print "D"
+	    int n=11;
+		for(int i=0;i<n;i++) 
+		{
+			for(int j=0;j<n;j++) {
+				
+				if(j==0||(i==0&&j!=(n-1))||(i==(n-1)&&j!=(n-1))||(j==(n-1)&&i!=0&&i!=(n-1))) {
+			System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
