6481c10144da1f3bf099939bfe01a8a1d704c92a
NagiReddy <pnagireddy444@gmail.com>
2022-10-30 09:51:34 +0530

    arrays frist commit

---
+++ arrays/src/arrays/Launch1.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+		/*
+		 * To create 2 dimensional regular array with 3 class and 5 students in each
+		 * class.
+		 */ 
+		
+		int[][] ar =new int[3][5];
+	}
+
+}
