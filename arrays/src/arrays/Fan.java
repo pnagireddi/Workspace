145ddd30b2b6d6f9d9bac291f4be44e815d141bf
NagiReddy <pnagireddy444@gmail.com>
2022-11-01 10:02:48 +0530

    arrays 3rd commit

---
+++ arrays/src/arrays/Fan.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Fan {
+	
+	// Concept of object type of arrays this class object is created at Launch9 class there we are initialized.
+	
+	String brand;
+	int cost;
+	int modelYear;
+	
+	@Override
+	public String toString() {
+		return "Fan [brand=" + brand + ", cost=" + cost + ", modelYear=" + modelYear + "]";
+	}
+	
+
+}
